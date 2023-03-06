0de02e7221aa64866d4f97c746b5af89635390d8
KonManov <126067187+KonManov@users.noreply.github.com>
2023-03-06 17:49:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string projectionType = Console.ReadLine();
+int numberOfRows = int.Parse(Console.ReadLine());
+int numberOfColumns = int.Parse(Console.ReadLine());
+double totalIncome = 0;
+
+switch(projectionType)
+{
+    case "Premier":
+        totalIncome = (numberOfRows * numberOfColumns) * 12.00; break;
+    case "Normal":
+        totalIncome = (numberOfColumns * numberOfRows) * 7.50; break;
+    case "Discount":
+        totalIncome = (numberOfRows * numberOfColumns) * 5.00; 
+        break;
+
+        default: break;
+}
+Console.WriteLine($"{totalIncome:f2}");
